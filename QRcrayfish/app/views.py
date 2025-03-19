a91f3d6478cc9c90033afcaf2f70309e50b38a6c
kirjuha161 <KIRILL-1B@MAIL.RU>
2025-03-19 19:23:27 +0300

    Co-authored-by: Neshkit <Neshkit@users.noreply.github.com>

--- QRcrayfish/app/views.py
+++ QRcrayfish/app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import qrcode
 from io import BytesIO
+import base64
 
 
 def index(request):
@@ -15,6 +16,7 @@ def generate_qr(request):
         buffer = BytesIO()
         qr.save(buffer, format="PNG")
         buffer.seek(0)
-        return HttpResponse(buffer, content_type="image/png")
+        qr_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
+        return render(request, "app/qr_result.html", {"qr_image": qr_image})
     else:
         return HttpResponse("No URL provided", status=400)
